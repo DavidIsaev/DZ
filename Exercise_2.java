134452a259a0777b26a9822c3545a35ada5fe897
DavidIsaev <davidisaevq@gmail.com>
2024-11-05 04:17:26 +0300

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -2,7 +2,7 @@ public class Exercise_2 {
     public static void main(String[] args) {
         int x=125,y=122;
         int a= x + y;
-        if (a>0) System.out.println("Сумма положительная");
+        if (a>=0) System.out.println("Сумма положительная");
         else System.out.println("Сумма отрицательная");
         System.out.println();
     }
